80f484b3e1809570ac7cd6f725dff43c6a50e893
Deddy Kosasih <deddy.kosasih@lm-c02fr15fq05r.modem>
2022-07-13 14:27:29 +1000

    fix: add termination character at the end of payload

--- VideoMicStatus/VideoMicStatus.ino
+++ VideoMicStatus/VideoMicStatus.ino
@@ -22,29 +22,29 @@ const short blue = 3;
 
 // Radio pipe addresses for the 2 nodes to communicate.
 const uint64_t pipes[2] = {
-  0xF2F0F0F0D3LL, 0xF1F0F0F0C3LL
-};
+    0xF2F0F0F0D3LL, 0xF1F0F0F0C3LL};
 
-//Door Status
-typedef enum {
-  NOMEETING = 0, MIC = 1, CAMERA = 2, MICANDCAMERA = 3
-}
-SwitchStats;
+// Door Status
+typedef enum
+{
+  NOMEETING = 0,
+  MIC = 1,
+  CAMERA = 2,
+  MICANDCAMERA = 3
+} SwitchStats;
 
 // FriendlyName of the door state
-const char* switchStatsFriendlyName[] = {
-  "No meeting\n", "Mic on\n", "Video on\n", "Video and mic on\n"
-};
+const char *switchStatsFriendlyName[] = {
+    "No meeting\n", "Mic on\n", "Video on\n", "Video and mic on\n"};
 
-//Status of the door currently
+// Status of the door currently
 SwitchStats switchStats;
-
 
 const int maxPayloadSize = 255;
 const int minPayloadSize = 4;
 const int packageSize = 32;
 const int payloadIncrementBy = 2;
-const char* packetSizeCode = "packet-size";
+const char *packetSizeCode = "packet-size";
 int nextPayloadSize = minPayloadSize;
 
 void setup(void)
@@ -54,7 +54,8 @@ void setup(void)
   configureRF();
 }
 
-void setInitialState() {
+void setInitialState()
+{
   pinMode(red, OUTPUT);
   pinMode(green, OUTPUT);
   pinMode(blue, OUTPUT);
@@ -66,7 +67,8 @@ void setInitialState() {
   switchStats = NOMEETING;
 }
 
-void configureRF() {
+void configureRF()
+{
   Serial.begin(57600);
   printf_begin();
 
@@ -76,7 +78,7 @@ void configureRF() {
   // We will be using the Ack Payload feature, so please enable it
   radio.enableAckPayload();
 
-  //All other setup
+  // All other setup
   radio.setDataRate(RF24_250KBPS);
   radio.setPALevel(RF24_PA_MAX);
   radio.setChannel(90);
@@ -97,14 +99,14 @@ void configureRF() {
 
 void loop(void)
 {
-    // if there is data ready
+  // if there is data ready
   if (radio.available())
   {
     String payload = getPayload();
-   
-    /* 
-    * NOTE: stop listening and send ack payload back when necessary
-    */
+
+    /*
+     * NOTE: stop listening and send ack payload back when necessary
+     */
 
     decideSwitchMode(payload);
     delay(1000);
@@ -112,10 +114,11 @@ void loop(void)
   runLightShow();
 }
 
-String getPayload(){
+String getPayload()
+{
   // Dump the payloads until we've gotten everything
   uint8_t len;
-  
+
   //[maxPayloadSize + 1];
   String finalMessage;
   unsigned long started_waiting_at = millis();
@@ -126,56 +129,67 @@ String getPayload(){
   while (radio.available() || radioIterator < packetCount)
   {
     char receive_payload[50];
-    if(radio.available()) {
+    if (radio.available())
+    {
       // Fetch the payload, and see if this was the last one.
       len = radio.getDynamicPayloadSize();
       radio.read(receive_payload, len);
-       // Spew it
+
+      receive_payload[len] = '\0';
+
+      // Spew it
       printf("Got payload size=%i value=%s\n\r", len, receive_payload);
     }
 
-    if (millis() - started_waiting_at > 2500) {
+    if (millis() - started_waiting_at > 2500)
+    {
       radioIterator = packetCount;
       printf("timeout; payload is now: %s\n\r", receive_payload);
     }
 
     // sender is sending packet size information
-    if(radioIterator == 0 && 
-      strncmp(receive_payload, packetSizeCode, sizeof(packetSizeCode)) == 0) {
-        strtok(receive_payload, ":");
-        packetCount = atoi(strtok(NULL, ":"));
-        printf("number of packets changed to: %i\n\r", packetCount);
+    if (radioIterator == 0 &&
+        strncmp(receive_payload, packetSizeCode, sizeof(packetSizeCode)) == 0)
+    {
+      strtok(receive_payload, ":");
+      packetCount = atoi(strtok(NULL, ":"));
+      printf("number of packets changed to: %i\n\r", packetCount);
 
-        radioIterator++;
+      radioIterator++;
     }
 
     // only concatenate string when packet count is still as the sender says
-    if(strncmp(receive_payload, packetSizeCode, sizeof(packetSizeCode)) != 0) {
+    if (strncmp(receive_payload, packetSizeCode, sizeof(packetSizeCode)) != 0 && strcmp(receive_payload, "") != 0)
+    {
       finalMessage.concat(receive_payload);
 
       radioIterator++;
     }
+
+    memset(receive_payload, 0, sizeof(receive_payload));
   }
   printf("no more data\n\r");
 
   return finalMessage;
 }
 
-void decideSwitchMode(String jsonString) {
+void decideSwitchMode(String jsonString)
+{
   DynamicJsonDocument doc(350);
-  char* jsonChar = const_cast<char*>(jsonString.c_str());
-  
+  char *jsonChar = const_cast<char *>(jsonString.c_str());
+
   printf("JSON covert: %s", jsonString.c_str());
   auto error = deserializeJson(doc, jsonChar);
-  if (error) {
-      Serial.print(F("deserializeJson() failed with code "));
-      Serial.println(error.c_str());
-      return;
+  if (error)
+  {
+    Serial.print(F("deserializeJson() failed with code "));
+    Serial.println(error.c_str());
+    return;
   }
 
   const int noMeetRed = doc["noMeeting"]["red"];
   const int micRed = doc["mic"]["red"];
-  
+
   printf("nomeet: %i, mic: %i; camera: %s\n\r", noMeetRed, micRed, doc["camera"]);
 
   // if (jsonString == "green")
@@ -192,15 +206,15 @@ void decideSwitchMode(String jsonString) {
   // }
 }
 
-void runLightShow() {
-  switch(switchStats) {
-    case MIC:
-      analogWrite(red, 255);
-      delay(1000);
-      analogWrite(red, 0);
-      delay(1000);
-      break;
-    }
+void runLightShow()
+{
+  switch (switchStats)
+  {
+  case MIC:
+    analogWrite(red, 255);
+    delay(1000);
+    analogWrite(red, 0);
+    delay(1000);
+    break;
+  }
 }
-
-
